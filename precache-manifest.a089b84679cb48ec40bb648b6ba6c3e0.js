162b1544e053eff49fb98a5782860e1f01c548c6
Isaac Cruz <isaac.clm1@gmail.com>
2021-08-19 21:29:51 -0300

    Updates

---
+++ precache-manifest.a089b84679cb48ec40bb648b6ba6c3e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044f7ae30d601feab3d29e9660864839",
+    "url": "/StarWarsPlanets/index.html"
+  },
+  {
+    "revision": "8ca7dbcbb1585e07d3bc",
+    "url": "/StarWarsPlanets/static/css/main.2bf20a5d.chunk.css"
+  },
+  {
+    "revision": "90f26bcf6b4d8b7836cb",
+    "url": "/StarWarsPlanets/static/js/2.3c64fa54.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StarWarsPlanets/static/js/2.3c64fa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca7dbcbb1585e07d3bc",
+    "url": "/StarWarsPlanets/static/js/main.70dd1364.chunk.js"
+  },
+  {
+    "revision": "822eb023d1f05efa6026",
+    "url": "/StarWarsPlanets/static/js/runtime-main.2511af5f.js"
+  },
+  {
+    "revision": "13be813cc3342d098d1d32bef726b7e0",
+    "url": "/StarWarsPlanets/static/media/star-wars.13be813c.gif"
+  }
+]);
